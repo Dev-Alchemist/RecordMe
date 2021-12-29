1ec4409b058ecab358d8803884987352b38f8d8c
waweru-steve <wawerusteve142@yahoo.com>
2021-12-29 14:04:21 +0300

    Added feature for fullScreen functionality

---
+++ app/src/main/java/com/steve/recorder/voicerecorder/utils/ViewUtils.kt
@@ -0,0 +1,24 @@
+package com.steve.recorder.voicerecorder.utils
+
+import android.app.Activity
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+
+fun Activity.hideSystemUI() {
+    if (isAboveR()) {
+        this.window.setDecorFitsSystemWindows(false)
+        this.window.insetsController?.let {
+            it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+            it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    } else {
+        @Suppress("DEPRECATION")
+        this.window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+    }
+}
