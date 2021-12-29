3f7ba95eae16e7a720946b4d148e5bfbb1fa1485
waweru-steve <wawerusteve142@yahoo.com>
2021-12-29 13:33:38 +0300

    Added util delay function

--- app/src/main/java/com/steve/recorder/voicerecorder/utils/CoroutinesHelper.kt
+++ app/src/main/java/com/steve/recorder/voicerecorder/utils/CoroutinesHelper.kt
@@ -1,4 +1,16 @@
 package com.steve.recorder.voicerecorder.utils
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 object CoroutinesHelper {
+
+    fun delayWithMain(delayTime: Long, work: () -> Unit) {
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(delayTime)
+            work()
+        }
+    }
 }
