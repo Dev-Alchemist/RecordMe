20a44837b271677f3a3f2ab6958498ed1659ffd4
waweru-steve <wawerusteve142@yahoo.com>
2021-12-29 12:57:48 +0300

    Configured data binding

--- app/src/main/java/com/steve/recorder/voicerecorder/ui/activities/MainActivity.kt
+++ app/src/main/java/com/steve/recorder/voicerecorder/ui/activities/MainActivity.kt
@@ -2,11 +2,22 @@ package com.steve.recorder.voicerecorder.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.steve.recorder.R
+import com.steve.recorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy {
+        findNavController(R.id.fragment)
+    }
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
     }
 }
