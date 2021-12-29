cff745879c64156a8a5adb831b39992cfc1d50c1
waweru-steve <wawerusteve142@yahoo.com>
2021-12-29 15:48:10 +0300

    Setup data hilt for database module

---
+++ app/src/main/java/com/steve/recorder/voicerecorder/di/DatabaseModule.kt
@@ -0,0 +1,34 @@
+package com.steve.recorder.voicerecorder.di
+
+import android.content.Context
+import androidx.room.Room
+import com.steve.recorder.voicerecorder.data.RecordDao
+import com.steve.recorder.voicerecorder.data.RecordDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+class DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun providesRecordDatabase(@ApplicationContext context: Context): RecordDatabase {
+        return Room.databaseBuilder(
+            context,
+            RecordDatabase::class.java,
+            RecordDatabase.DATABASE_NAME
+        ).fallbackToDestructiveMigration().build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesRecordDao(recordDatabase: RecordDatabase): RecordDao {
+        return recordDatabase.dao()
+    }
+}
