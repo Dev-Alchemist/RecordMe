a57eaf7ef0f7c7f912277f47cd2cb9aed3e5f80d
waweru-steve <wawerusteve142@yahoo.com>
2021-12-29 17:08:10 +0300

    Setup permissions checker

--- app/src/main/java/com/steve/recorder/voicerecorder/ui/activities/MainActivity.kt
+++ app/src/main/java/com/steve/recorder/voicerecorder/ui/activities/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.steve.recorder.voicerecorder.ui.activities
 
+import android.Manifest
+import android.Manifest.permission.RECORD_AUDIO
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
@@ -16,16 +21,44 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var mainBinding: ActivityMainBinding
+    private val permList: Array<String> = arrayOf(
+        RECORD_AUDIO,
+        WRITE_EXTERNAL_STORAGE
+    )
+
+    companion object {
+        private const val PERMISSION_CODE = 121
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+        if (!checkForPermission(RECORD_AUDIO)) {
+            askPermission(permList)
+
+        }
+        if (!checkForPermission(WRITE_EXTERNAL_STORAGE)) {
+            askPermission(permList)
+        }
     }
 
     override fun onStart() {
         super.onStart()
         setFullScreenForNotch()
+    }
+
+    private fun checkForPermission(permission: String) : Boolean {
+
+        return ActivityCompat.checkSelfPermission(
+            this@MainActivity,
+            permission
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun askPermission(permList: Array<String>) {
+        ActivityCompat.requestPermissions(this@MainActivity, permList, PERMISSION_CODE)
     }
 
     override fun onSupportNavigateUp(): Boolean {
