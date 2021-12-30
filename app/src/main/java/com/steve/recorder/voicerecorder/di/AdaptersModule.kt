a4e493c9796194c7f59ed325d72c170f8ca0952b
waweru-steve <wawerusteve142@yahoo.com>
2021-12-30 12:24:25 +0300

    Dependency injection for the Adapter class

--- app/src/main/java/com/steve/recorder/voicerecorder/di/AdaptersModule.kt
+++ app/src/main/java/com/steve/recorder/voicerecorder/di/AdaptersModule.kt
@@ -1,4 +1,18 @@
 package com.steve.recorder.voicerecorder.di
 
+import com.steve.recorder.voicerecorder.adapters.VoiceRecordAdapter
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+
+@InstallIn(ActivityComponent::class)
+@Module
 class AdaptersModule {
+
+    @Provides
+    fun providesVoiceAdapter(): VoiceRecordAdapter {
+        return VoiceRecordAdapter()
+    }
 }
